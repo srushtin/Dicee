add46c8728ae256e07023d0ddeeb905b68ddf57e
srushtin <45654631+srushtin@users.noreply.github.com>
2020-05-25 22:59:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function reload() {
+  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+  var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+  var randomDiceImg1 = "images/dice" + randomNumber1 + ".png";
+  var randomDiceImg2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", randomDiceImg1);
+  document.querySelectorAll("img")[1].setAttribute("src", randomDiceImg2);
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+}
+
+document.querySelector("button").addEventListener("click", function() {
+  reload();
+});
